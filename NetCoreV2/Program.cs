ff56603b6517e8d21a759d05914083e5ab3cc234
Ramazan HABER <ramazanhaberr@gmail.com>
2023-01-01 01:57:52 +0300

    dd

--- NetCoreV2/Program.cs
+++ NetCoreV2/Program.cs
@@ -9,11 +9,14 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>{
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "myapi v1");
+    }) ;
 }
 
 app.UseHttpsRedirection();
